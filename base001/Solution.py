03bedb30145f1860d30f31562b3c42220ad2b753
xsing <2672946694@qq.com>
2017-01-14 15:00:49 +0800

    update 1

--- base001/Solution.py
+++ base001/Solution.py
@@ -3,20 +3,31 @@
 # 在一个二维数组中，每一行都按照从左到右递增的顺序排序，
 # 每一列都按照从上到下递增的顺序排序。请完成一个函数，
 # 输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+import Solution as Solution
+
 
 class Solution:
     # array 二维列表
-    @staticmethod
-    def Find(target, array):
 
-        row = 0
-        for i, arr in enumerate(array):
-            
-            m = int(i/2)
-            if target==arr[m]:
+    def Find(target, array):
+        index = len(array)-1
+        jndex = 0
+        while True:
+            if jndex == len(array[0]) or index < 0:
+                return False
+            print(array[index][jndex])
+            if target == array[index][jndex]:
+                print("存在目标:" + str(target) + " i:" + str(index) + " j:" + str(jndex))
                 return True
-                pass
-            elif target>arr[m]:
-                pass
+            elif target < array[index][jndex]:
+                index -= 1
             else:
-                pass
+                jndex += 1
+        return False
+
+
+
+
+
+
+
---
+++ base001/装饰器.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+import functools
+
+
+def log(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        print("call %s():" % func.__name__)
+        return func(*args, **kw)
+
+    return wrapper
+
+
+@log
+def now():
+    print("2017年01月14日10:52:02")
+
+
+now()
+print(now.__name__)
+
+
+# 2
+
+def log2(text):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            print(text)
+            return func(*args, *kw)
+
+        return wrapper
+
+    return decorator
+
+
+@log2('execute')
+def now2():
+    print('now2() 2017年01月14日11:04:04')
+
+
+now2()
+print(now2.__name__)
+
+# 3
+def log(text=''):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            if len(text)>0:
+                print(text)
+            else:
+                print('')
+            return func(*args, *kw)
+
+        return wrapper
+
+    return decorator
+
+@log('log  haha')
+def now():
+    print("hello decorator!")
+
+now()
