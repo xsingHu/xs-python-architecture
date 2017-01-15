bcdbdecb7236913ee6c926e81650b9f5ae86f36f
xsing <2672946694@qq.com>
2017-01-15 12:33:57 +0800

    update 2

--- practice/Solution.py
+++ practice/Solution.py
@@ -1,9 +1,9 @@
-# -*- coding:utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 # 在一个二维数组中，每一行都按照从左到右递增的顺序排序，
 # 每一列都按照从上到下递增的顺序排序。请完成一个函数，
 # 输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
-import Solution as Solution
 
 
 class Solution:
