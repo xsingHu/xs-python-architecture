d809a53b41cef6bf82a9ac917a1a26b012d0c1bd
xsing <2672946694@qq.com>
2017-11-20 18:44:55 +0800

    函数式编程

--- reference/base/03函数式编程.py
+++ reference/base/03函数式编程.py
@@ -1,24 +1,161 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-1. 切片
+Functional Programming 函数式编程
 
-2.迭代
+函数是Python内建支持的一种封装，我们通过把大段代码拆成函数，通过一层一层的函数调用，就可以把复杂任务分解成简单的任务，这种分解可以称之为面向过程的程序设计。函数就是面向过程的程序设计的基本单元。
 
-3.列表生成式
 
-4.生成器
+函数式编程就是一种抽象程度很高的编程范式，
 
-5.迭代器
+纯粹的函数式编程语言编写的函数没有变量，
+因此，任意一个函数，只要输入是确定的，输出就是确定的，这种纯函数我们称之为没有副作用。
 
+而允许使用变量的程序设计语言，由于函数内部的变量状态不确定，同样的输入，可能得到不同的输出，
+因此，这种函数是有副作用的。
+
+函数式编程的一个特点就是，允许把函数本身作为参数传入另一个函数，还允许返回一个函数！
+
+Python对函数式编程提供部分支持。由于Python允许使用变量，因此，Python不是纯函数式编程语言。
 
 """
-def my_abs(x):
-    if not isinstance(x, (int, float)):
-        raise TypeError('bad operand type')
-    if x >= 0:
-        return x
-    else:
-        return -x
+# 高阶函数 : 以函数作为参数的函数
 
-print(my_abs(100))
+# 变量可以指向函数 :函数本身也可以赋值给变量
+# 函数名也是变量
+
+# Python内建了map()和reduce()函数。
+
+# map()函数接收两个参数，一个是函数，一个是Iterable，map将传入的函数依次作用到序列的每个元素，并把结果作为新的Iterator返回
+from functools import reduce
+
+import functools
+
+
+def f(x):
+    return x * x
+
+
+r = map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(list(r))
+
+
+# reduce把一个函数作用在一个序列[x1, x2, x3, ...]上，这个函数必须接收两个参数，reduce把结果继续和序列的下一个元素做累积计算
+
+# reduce(f, [x1, x2, x3, x4]) = f(f(f(x1, x2), x3), x4)
+
+def add(x, y):
+    return x + y
+
+
+print(reduce(add, [1, 2, 3, 4, 5]))
+
+
+# int()
+def char2num(s):
+    return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
+
+
+def str2int(s):
+    return reduce(lambda x, y: x * 10 + y, map(char2num, s))
+
+
+# 练习 把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字。输入：['adam', 'LISA', 'barT']，输出：['Adam', 'Lisa', 'Bart']
+def fLower(s):
+    if isinstance(s, str):
+        return s[0].upper() + s[1:].lower()
+    else:
+        return None
+
+L = ['adam', 'LISA', 'barT']
+print(list(map(fLower,L)))
+
+
+# Python内建的filter()函数用于过滤序列。filter()也接收一个函数和一个序列。和map()不同的是，filter()把传入的函数依次作用于每个元素，然后根据返回值是True还是False决定保留还是丢弃该元素。
+def is_odd(n):
+    return n % 2 == 1
+
+list(filter(is_odd, [1, 2, 4, 5, 6, 9, 10, 15]))
+
+# 素数
+def _odd_iter():
+    n = 1
+    while True:
+        n = n + 2
+        yield n
+
+def _not_divisible(n):
+    return lambda x: x % n > 0
+
+def primes():
+    yield 2
+    it = _odd_iter() # 初始序列
+    while True:
+        n = next(it) # 返回序列的第一个数
+        yield n
+        it = filter(_not_divisible(n), it) # 构造新序列
+
+for n in primes():
+    if n < 1000:
+        print(n)
+    else:
+        break
+
+# 函数作为返回值 闭包
+def lazy_sum(*args):
+    def sum():
+        ax = 0
+        for n in args:
+            ax = ax + n
+        return ax
+    return sum
+
+print(lazy_sum(*[1,3,4])())
+"""
+
+"""
+# 匿名函数
+"""
+关键字lambda表示匿名函数，冒号前面的x表示函数参数。
+匿名函数有个限制，就是只能有一个表达式，不用写return，返回值就是该表达式的结果。
+用匿名函数有个好处，因为函数没有名字，不必担心函数名冲突。此外，匿名函数也是一个函数对象，也可以把匿名函数赋值给一个变量，再利用变量来调用该函数
+"""
+# 装饰器
+"""
+假设我们要增强now()函数的功能，比如，在函数调用前后自动打印日志，但又不希望修改now()函数的定义，这种在代码运行期间动态增加功能的方式，称之为“装饰器”（Decorator）
+"""
+def log(func):
+    def wrapper(*args, **kw):
+        print('call %s():' % func.__name__)
+        return func(*args, **kw)
+    return wrapper
+
+@log
+def now():
+    print('2015-3-25')
+
+now()
+
+
+# 偏函数 Partial function
+"""
+当函数的参数个数太多，需要简化时，使用functools.partial可以创建一个新的函数，这个新函数可以固定住原函数的部分参数，从而在调用时更简单。
+"""
+int2 = functools.partial(int, base=2)
+print(int2('100'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
